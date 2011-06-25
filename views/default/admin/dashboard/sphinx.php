1aa3a55bd9d1d4aa25eb8f181bf6c976440f6786
Evan Winslow <evan@elgg.org>
2011-06-25 01:50:53 -0700

    Added basic status page for sphinx

--- views/default/admin/dashboard/sphinx.php
+++ views/default/admin/dashboard/sphinx.php
@@ -1,3 +1,34 @@
 <?php
 
-echo "Nothing to see here yet.";
+$cl = new SphinxClient();
+$cl->setServer('localhost', 9312);
+$status = $cl->Status();
+
+if ($status) {
+	echo elgg_view('output/longtext', array(
+		'value' => 'IO and CPU counters will only be available if searchd was started with --iostats and --cpustats switches respectively.',
+		'class' => 'elgg-message elgg-state-notice',
+	));
+	
+	echo "<table class=\"elgg-table\">";
+	foreach ($status as $row) {
+		echo "<tr><th>{$row[0]}</th><td>{$row[1]}</td></tr>";
+	}
+	echo "</table>";
+} else {
+	
+	echo elgg_view('output/longtext', array(
+		'value' => $cl->GetLastError(),
+		'class' => 'elgg-message elgg-state-error',
+	));
+	
+	global $CONFIG;
+	
+	echo "Please check that searchd is running:";
+	echo "<pre>";
+	echo "searchd --stop\n";
+	echo "indexer --all --config {$CONFIG->dataroot}sphinx/sphinx.conf\n";
+	echo "searchd --config {$CONFIG->dataroot}sphinx/sphinx.conf\n";
+	echo "</pre>";
+	
+}
